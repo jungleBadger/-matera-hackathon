473ce1b6004d979d80c6d9d8863bd0ebfd4364d4
Alexia Balionis <balionisalexia@gmail.com>
2016-12-03 00:56:33 -0200

    File fix

---
+++ client/scripts/main.script.js
@@ -0,0 +1,9 @@
+(function () {
+    "use strict";
+
+    var controller = require("./login.controller")(),
+        factory = require("./login.factory")();
+
+    controller.b();
+    // console.log(controller);
+}());
