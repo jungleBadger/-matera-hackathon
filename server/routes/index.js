094117b4ecbfc3686c3d164a6f6555c43444b7e5
Nickolas Leal Fernandes <nickolas@cit>
2016-12-04 04:14:26 -0200

    [nickolas] MERGE

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,16 +1,6 @@
 (function () {
     "use strict";
 
-<<<<<<< HEAD
-    var loginRoutes = require('./partials/loginHandler'),
-        accountRoutes = require('./partials/accountHandler'),
-        balanceRoutes = require('./partials/balanceHandler'),
-        statementRoutes = require('./partials/statementHandler'),
-        deviceRoutes = require('./partials/deviceHandler'),
-        brokerRoutes = require('./partials/brokerHandler'),
-        driverRoutes = require("./partials/driverHandler"),
-        paymentRoutes = require("./partials/paymentHandler");
-=======
     var loginRoutes = require("./partials/loginHandler"),
         accountRoutes = require("./partials/accountHandler"),
         balanceRoutes = require("./partials/balanceHandler"),
@@ -19,8 +9,8 @@
         brokerRoutes = require("./partials/brokerHandler"),
         truckRoutes = require("./partials/truckHandler"),
         driverRoutes = require("./partials/driverHandler"),
-        tripsRoutes = require("./partials/tripsHandler");
->>>>>>> 2ee711c94d4cd4d3050101d491573dd019b2e8f4
+        tripsRoutes = require("./partials/tripsHandler"),
+        paymentRoutes = require("./partials/paymentHandler");
 
     module.exports = function (app, io, request, materaMP, iotf_connections, iotf_configs, passport, Cloudant, UserSchema) {
         loginRoutes(app, passport);
